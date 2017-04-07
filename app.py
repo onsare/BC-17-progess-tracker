d5d15818ef69b4bbabd89f9cdc9994dd2c1cff86
machariamarigi <machariamarigi@gmail.com>
2017-04-07 14:32:28 +0300

    Implemented inserting task feature

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import logic
 
 import ui
+import model
 
 ui.print_menu()
 
 user_input = logic.user_input("Please Enter your selection")
 if user_input == "1":
-  pass
-  # insert task
+  task = logic.user_input("Please enter your task: ")
+  model.insert_task(task)
+  #makes program pause 
+  user_input = logic.user_input("Press any key to continue")
 elif user_input == "2":
   pass
-  course = logic.user_input("Please enter your task")
+  course = logic.user_input("Please enter your task to be queried")
   # query dictionary
 else:
   ui.print_message("Good-bye Thanks for Using Progress Tracker")
