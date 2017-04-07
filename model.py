7611fea41cd32cf79d536910ddc565b0a38388bd
machariamarigi <machariamarigi@gmail.com>
2017-04-07 14:45:08 +0300

    Fixed syntax error on model

--- model.py
+++ model.py
@@ -10,8 +10,8 @@ def password():
 def get_tasks():
   return db.tasks
 
-def get_task(task)
-    return task in get_tasks() 
+def get_task(task):
+  return task in get_tasks() 
 
 def get_progress(task):
   if task in db.tasks:
