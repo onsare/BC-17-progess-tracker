f009d045fb5026ab6933d0350e265a557f473f06
machariamarigi <machariamarigi@gmail.com>
2017-04-07 14:22:34 +0300

    Implemented model, version 1.0

---
+++ model.py
@@ -0,0 +1,14 @@
+db = {"username" = '', "password" = "", "tasks" = {}}
+
+def username():
+  
+  return db.username
+
+def password():
+  return db.password
+
+def get_tasks():
+  return db.tasks
+
+def get_task(task)
+    return task in get_tasks() 
